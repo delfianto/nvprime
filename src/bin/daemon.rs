ac913abe126c70a3dc0d3547f93266193abb20a1
Dwi Elfianto <2359896+delfianto@users.noreply.github.com>
2025-11-25 12:17:47 +0700

    More daemon stub code

--- src/bin/daemon.rs
+++ src/bin/daemon.rs
@@ -1,5 +1,18 @@
 const LOCK_FILE: &str = "/tmp/nvprime-sys.lock";
 
-pub fn new() {
-    print!("Daemon Initialized")
+struct NvPrimeDaemon {
+    lock_file: String,
+}
+
+impl NvPrimeDaemon {
+    pub fn new() {
+        println!("Daemon Initialized")
+    }
+    pub fn hello() {
+        println!("Daemon Initialized")
+    }
+}
+
+pub fn main() {
+    let daemon = NvPrimeDaemon::new();
 }
