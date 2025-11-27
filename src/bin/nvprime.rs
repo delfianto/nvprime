518d57fb857c4a22e47f8723b32ad48416d5f7a5
Dwi Elfianto <dwi.elfianto@gmail.com>
2025-11-27 19:08:04 +0700

    Finally working executable detection

--- src/bin/nvprime.rs
+++ src/bin/nvprime.rs
@@ -5,6 +5,8 @@ use primers::runner::Launcher;
 
 fn main() -> Result<()> {
     logging::init(true)?;
+
+    // Remove 1st argument as this is the nvprime itself
     let args: Vec<String> = std::env::args().skip(1).collect();
 
     if args.is_empty() {
@@ -13,12 +15,7 @@ fn main() -> Result<()> {
     }
 
     info!("Starting nvprime");
-    // debug!("Command line arguments: {:?}", args);
-
-    // Load configuration
-    // debug!("Loading configuration...");
     let config = Config::load()?;
-    // info!("Configuration loaded successfully");
 
     // NVIDIA GPU initialization
     match NvGpu::init(config.gpu.gpu_uuid.clone()) {
@@ -35,35 +32,10 @@ fn main() -> Result<()> {
     // debug!("Running init hooks");
     // Hooks::run_init(&config, config.hooks.init.as_deref())?;
 
-    // Get executable name
-    // let exe_name = get_executable_name(&args);
-    // debug!("Detected executable name: '{}'", exe_name);
-
-    // let env_builder = EnvBuilder::new();
-
-    // Merge global environment variables
-    // debug!(
-    //     "Applying global environment variables: {} vars",
-    //     config.environments.global.len()
-    // );
-    // env_builder.merge_global(&config.environments.global);
-
-    // Merge executable-specific environment variables
-    // if let Some(exe_config) = config.environments.executables.get(&exe_name) {
-    //     info!("Found executable-specific configuration for '{}'", exe_name);
-    //     debug!("Applying {} executable-specific env vars", exe_config.len());
-    //     env_builder.merge_executable(Some(exe_config));
-    // } else {
-    //     debug!("No executable-specific configuration for '{}'", exe_name);
-    //     env_builder.merge_executable(None);
-    // }
-
     debug!("Launching process...");
-    // let final_env = env_builder.with_config(&config, &exe_name);
-    let mut launcher = Launcher::new(args);
+    let mut launcher = Launcher::new(args, &config);
 
     // Start and wait for completion
-    // debug!("Launching process");
     let exit_code = launcher.execute()?;
 
     // Run shutdown hooks
